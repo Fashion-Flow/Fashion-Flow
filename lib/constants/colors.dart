502b2ec4e700b41a189c34db0f100c263a7339c1
KatayR <rasitarslan@outlook.com>
2023-06-14 21:03:34 +0300

    Added constants

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ffColors {
-  static Color Black = Color(0xFF191E33);
-  static Color Blue = Color(0xFF4361EE);
-  static Color DarkBlue = Color(0xFF3A0CA3);
-  static Color Purple = Color(0xFF7209B7);
-  static Color Gray = Color(0xFF707070);
-  static Color Pink = Color(0xFFF72585);
+  static const Color Black = Color(0xFF191E33);
+  static const Color Blue = Color(0xFF4361EE);
+  static const Color DarkBlue = Color(0xFF3A0CA3);
+  static const Color Purple = Color(0xFF7209B7);
+  static const Color Gray = Color(0xFF707070);
+  static const Color Pink = Color(0xFFF72585);
 }
